bf0e31962a397e1fc26eb9337e25e3956b91a5ac
Gary Bortosky <garybort@gmail.com>
2020-01-05 01:58:12 -0600

    add config type

--- packages/karma-timer-reporter/src/index.ts
+++ packages/karma-timer-reporter/src/index.ts
@@ -1,117 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/member-ordering, sort-keys */
 import { SpecResult, compare } from './SpecResult';
+import { RunResults, Browser, Logger, Helper, LoggerFactory } from './Karma';
 
-interface RunResults {
-  success: number;
-  failed: number;
-  skipped: number;
-  error: boolean;
-  disconnected: boolean;
-  exitCode: number;
-}
-
-interface BrowserResults {
-  id: string;
-  fullName: string;
-  name: string;
-  state: 'EXECUTING' | 'UMM';
-  lastResult: {
-    startTime: number;
-    total: number;
-    success: number;
-    failed: number;
-    skipped: number;
-    totalTime: number;
-    netTime: number;
-    error: boolean;
-    disconnected: boolean;
-  };
-  disconnectsCount: number;
-  noActivityTimeout: number;
-  disconnectDelay: number;
-}
-
-interface Helper {
-  browserFullNameToShort: Function;
-  isDefined: Function;
-  mmPatternWeight: Function;
-  mmComparePatternWeights: Function;
-  isFunction: Function;
-  isString: Function;
-  isObject: Function;
-  isArray: Function;
-  isNumber: Function;
-  isUrlAbsolute: Function;
-  camelToSnake: Function;
-  ucFirst: Function;
-  dashToCamel: Function;
-  arrayRemove: Function;
-  merge: Function;
-  formatTimeInterval: Function;
-  normalizeWinPath: Function;
-  mkdirIfNotExists: Function;
-  defer: Function;
-  saveOriginalArgs: Function;
-  restoreOriginalArgs: Function;
-}
-
-const rrs: RunResults = {
-  success: 2,
-  failed: 0,
-  skipped: 41,
-  error: false,
-  disconnected: false,
-  exitCode: 0,
-};
-
-const a: BrowserResults = {
-  id: '90308918',
-  fullName:
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-  name: 'Chrome 79.0.3945 (Windows 10.0.0)',
-  state: 'UMM',
-  lastResult: {
-    startTime: 1578186014680,
-    total: 43,
-    success: 2,
-    failed: 0,
-    skipped: 38,
-    totalTime: 0,
-    netTime: 50,
-    error: false,
-    disconnected: false,
-  },
-  disconnectsCount: 0,
-  noActivityTimeout: 30000,
-  disconnectDelay: 2000,
-};
-
-interface LoggerFactory {
-  create(name: string): Logger;
-
-}
-
-interface Logger {
-  warn(message: string|number): void;
+interface TimerConfig {
+  tbd: number;
 }
 
 class TimerReporter {
-  private specs: Map<string, SpecResult> = new Map();
+  protected static $inject = ['helper', 'logger', 'config.timerReporter'];
   protected adapters: Function[] = [];
   protected log: Logger;
-  public static $inject = ['helper', 'logger', 'config.timerReporter'];
+  private specs: Map<string, SpecResult> = new Map();
   constructor(
     protected helper: Helper,
     protected logger: LoggerFactory,
-    protected config: any
+    protected config: TimerConfig
   ) {
     this.log = logger.create('reporter.timer');
   }
-  public onBrowserComplete(browser: BrowserResults): void {
+  public onBrowserComplete(browser: Browser): void {
     console.log(browser.fullName);
+    console.log(this.config.tbd);
   }
-  public onSpecComplete(browser: any, result: SpecResult): void {
-    console.log(browser);
+  public onSpecComplete(browser: Browser, result: SpecResult): void {
+    console.log(browser.id, 'is fun');
     // this.log.warn(JSON.stringify(browser));
     // this.log.warn(JSON.stringify(result));
     this.specs.set(result.id, result);
